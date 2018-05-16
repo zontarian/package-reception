307f0ac2389d275504a0cc9915d78a0b2ea889cd
Walter Gamba <walter@iakta.it>
2018-05-16 12:10:25 +0200

    mod version

--- scroller/tile.lua
+++ scroller/tile.lua
@@ -48,6 +48,7 @@ local function draw_scroller(x, y, w, h)
     print("x ".. x .. " y " .. y .. " w ".. w .. ' h ' .. h)
 --    hh = 200
 --    scissors.set(x, y, x+w, y+hh)
+    scissors.set(WIDTH-50, 0, WIDTH, HEIGHT-300)
 
     local now = sys.now()
     local delta = now - last
